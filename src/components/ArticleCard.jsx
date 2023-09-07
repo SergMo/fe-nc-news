9e875f55f78f138134fae797f1b08b76c3ab017d
Serg M <msbox16@gmail.com>
2023-09-07 11:46:58 +0100

    task 7: vote on an article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
 
 export default function ArticleCard({ article }) {
+	const [votes, setVotes] = useState(article.votes);
+	const [isError, setIsError] = useState(false);
+	console.log(votes);
+
+	const handleVote = (voteChange) => {
+		setVotes(votes + voteChange); // Optimistic change in votes
+		axios
+			.patch(`https://news-project-baar.onrender.com/api/articles/${article.article_id}`, {
+				inc_votes: voteChange
+			})
+			.then(({ data }) => {
+				console.log(data.article.votes);
+				setVotes(data.article.votes);
+			})
+			.catch((err) => {
+				console.error('Error updating the vote. Please try again: ', err);
+				setVotes(votes - voteChange);
+				setIsError(true)
+			});
+	};
 
 	return (
 		<div className="article-card">
@@ -7,8 +29,11 @@ export default function ArticleCard({ article }) {
 			<p>Author: {article.author}</p>
 			<p>Topic: {article.topic}</p>
 			<p>Date: {article.created_at}</p>
-			<p>Votes: {article.votes}</p>
 			<img src={article.article_img_url} alt={article.title} />
+			<p>Votes !!!: {votes}</p>
+			<button onClick={() => { handleVote(1) }}>Upvote</button>
+			<button onClick={() => { handleVote(-1) }}>Downvote</button>
+			{isError && <p>{isError}</p>}
 		</div>
 	)
 }
